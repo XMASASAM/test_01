b15cba1fda71c06c29a98ee5e1ef22a4544cde7c
MasaoMasao <83535394+XMASASAM@users.noreply.github.com>
2024-11-29 19:34:33 +0900

    Create unility9.cpp

---
+++ unility9.cpp
@@ -0,0 +1,42 @@
+#include"f4m.h"
+
+void qbs::f4m::check_error(int ret) {
+	char buf[255];
+	int r = av_strerror(ret, buf, 255);
+	if (r == 0) {
+		printf("qbs_check_error:: %d : %s\n", ret, buf);
+	}
+	else {
+		printf("qbs_check_error:: %d : not found error code\n", ret);
+	}
+}
+
+bool detect_video_frame(AVFrame* frame) {
+	if (frame->width <= 0 || frame->height <= 0)return false;
+	return true;
+}
+
+void check_error(int ret, const char* com) {
+	char buf[255];
+	int r = av_strerror(ret, buf, 255);
+	if (r == 0) {
+		printf("check_error: func=%s : ret=%d : str=%s\n", com, ret, buf);
+	}
+	else {
+		printf("check_error: func=%s : ret=%d : str=%s\n", com, ret, "ok");
+	}
+}
+
+bool str_null_or_empty(const char* str) {
+	if (str == nullptr)return true;
+	if (strncmp(str, "", 1) == 0)return true;
+	return false;
+}
+
+
+void av_frame_deleter(AVFrame* frame) {
+	av_frame_free(&frame);
+}
+share_frame make_share_frame(AVFrame* frame) {
+	return std::shared_ptr<AVFrame>(frame, av_frame_deleter);
+}
